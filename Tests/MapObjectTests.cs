9af53f30d3155363e0e60b17bb40aff57c692f59
Stanislav Kozlov <llarean@ya.ru>
2025-09-29 16:29:32 +0300

    [AI] Add test project with initial unit tests
    Introduced a new Tests project with NUnit and related dependencies.
    Added MapObjectTests and a temporary test class.
    Updated .gitignore to exclude test build artifacts, and modified the solution and main project files to include and reference the new test project.

---
+++ Tests/MapObjectTests.cs
@@ -0,0 +1,44 @@
+﻿using kings_bounty_console.Maps;
+
+namespace Tests;
+
+[TestFixture]
+public class MapObjectTests
+{
+    [Test]
+    public void Ctor_PassArray_DoesNotThrow()
+    {
+        char[,] map =
+        {
+            { '1', '@', '/' },
+            { 's', '-', '"' }
+        };
+        
+        Assert.DoesNotThrow(() =>
+        {
+            new MapObject(map);
+        });
+    }
+    
+    [Test]
+    public void Ctor_PassEmptyArray_DoesNotThrow()
+    {
+        char[,] map = new char[,]{};
+        
+        Assert.DoesNotThrow(() =>
+        {
+            new MapObject(map);
+        });
+    }
+    
+    [Test]
+    public void Ctor_PassNull_DoesNotThrow()
+    {
+        char[,] map = null;
+
+        Assert.DoesNotThrow(() =>
+        {
+            new MapObject(map);
+        });
+    }
+}
---
+++ Tests/TestingTemp.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework.Legacy;
+
+namespace Tests;
+
+[TestFixture]
+public class TestingTemp
+{
+    [Test]
+    public void Test()
+    {
+        var temp = 1 + 1;
+        ClassicAssert.True(temp == 2);
+    }
+}
